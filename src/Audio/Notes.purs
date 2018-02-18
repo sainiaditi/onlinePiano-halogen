463544882b6e9130cb9838ee25bf61721b17a2fe
Aditi Saini <saini.aditi@hotmail.com>
2018-02-18 23:25:04 +0530

    added piano notes

--- src/Audio/Notes.purs
+++ src/Audio/Notes.purs
@@ -9,10 +9,39 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 
 
 freq :: Note -> Frequency
-freq A = 440.000
-freq B = 493.900
-freq C = 261.600
-freq D = 293.700
-freq E = 329.600
-freq F = 349.200
-freq G = 392.000
+freq A3 = 220.000
+freq B3 = 246.900
+freq Bb3 = 233.100
+freq C3 = 130.800
+freq CS3 = 138.600
+freq D3 = 146.800
+freq E3 = 164.800
+freq Eb3 = 155.600
+freq F3 = 174.600
+freq FS3 = 185.000
+freq G3 = 196.000
+freq GS3 = 207.700
+freq A4 = 440.000
+freq B4 = 493.900
+freq Bb4 = 466.200
+freq C4 = 261.600
+freq CS4 = 277.2
+freq D4 = 293.700
+freq E4 = 329.600
+freq Eb4 = 311.100
+freq F4 = 349.200
+freq FS4 = 370.000
+freq G4 = 392.000
+freq GS4 = 392.000
+freq A5 = 880.000
+freq B5 = 987.800
+freq Bb5 = 932.300
+freq C5 = 523.300
+freq CS5 = 554.400
+freq D5 = 587.300
+freq E5 = 659.300
+freq Eb5 = 622.300
+freq F5 = 698.500
+freq FS5 = 740.000
+freq G5 = 784.000
+freq GS5 = 784.000
--- src/Audio/Types.purs
+++ src/Audio/Types.purs
@@ -16,4 +16,6 @@ type Patterns = Array Pattern
 
 data Pattern = X | P | R
 
-data Note = A | B | C | D | E | F | G
+data Note = A3 | B3 | Bb3 | C3 | CS3 | D3 | E3 | Eb3 | F3 | FS3 | G3 | GS3 |
+            A4 | B4 | Bb4 | C4 | CS4 | D4 | E4 | Eb4 | F4 | FS4 | G4 | GS4 |
+            A5 | B5 | Bb5 | C5 | CS5 | D5 | E5 | Eb5 | F5 | FS5 | G5 | GS5 
